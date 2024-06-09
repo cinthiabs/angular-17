e29c099588fbe13e5c80d7ad8ec9017fc7c3e22a
cinthiabs <cinthiabarbosa8d@outlook.com>
2024-06-08 21:22:42 -0300

    form

--- src/app/grud-angular/grud-angular.component.ts
+++ src/app/grud-angular/grud-angular.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-grud-angular',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './grud-angular.component.html',
   styleUrl: './grud-angular.component.css'
 })
 export class GrudAngularComponent {
 
+  //Obj de formulario
+  formulario = new FormGroup({
+    nome: new FormControl('',[Validators.required, Validators.minLength(3)]),
+    idade: new FormControl(null,[Validators.required, Validators.min(0), Validators.max(120)]),
+    cidade: new FormControl('',[Validators.required, Validators.minLength(3)])
+  });
 }
